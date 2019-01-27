10868305ef07bfb395fee48258d46834efd77055
Durieu Emilien <emilien@qover.me>
2019-01-27 14:47:52 +0100

    use more standard page, fix disappering search query bug

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ import Header from './Header';
 import Home from './pages/Home';
 import LineUp from './pages/LineUp';
 import StandardPage from './pages/StandardPageContainer';
-import About from './pages/About';
-import Tickets from './pages/Tickets';
 import Breakpoint from './helperComponents/Breakpoint';
 
 const ContentContainer = styled.div`
@@ -32,13 +30,15 @@ const App = () => (
             path="/:url*"
             exact
             strict
-            render={({ location }) => <Redirect to={`${location.pathname}/`} />}
+            render={({ location }) => (
+              <Redirect to={{ ...location, pathname: `${location.pathname}/` }} />
+            )}
           />
           <Route path="/accueil" component={Home} />
           <Route path="/programmation/:band?" render={route => <LineUp route={route} />} />
           <Route path="/infos-pratiques" render={() => <StandardPage slug="infos-pratiques" />} />
           <Route path="/a-propos" render={() => <StandardPage slug="a-propos" />} />
-          <Route path="/contributions" component={() => 'Concontritribubutiontion'} />
+          <Route path="/contributions" render={() => <StandardPage slug="contributions" />} />
           <Route from="/tickets" render={() => <StandardPage slug="tickets" />} />
           <Redirect from="/" to="/accueil" />
           <Route component={() => <h1>Quatre Sans Quatre</h1>} />
